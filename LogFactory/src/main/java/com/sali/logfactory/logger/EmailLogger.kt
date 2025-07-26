36a21f2eeaf01184f19526326457a40b855b5882
Ali Nabavi <arz1379n@gmail.com>
2025-07-26 23:07:34 +0330

    Implement EmailLogger and its related configuration (SmtpConfig class)

---
+++ LogFactory/src/main/java/com/sali/logfactory/logger/EmailLogger.kt
@@ -0,0 +1,140 @@
+package com.sali.logfactory.logger
+
+import android.content.Context
+import android.util.Log
+import com.sali.logfactory.models.LogConfig
+import com.sali.logfactory.models.LogEntry
+import com.sali.logfactory.models.SmtpConfig
+import com.sali.logfactory.models.ThresholdType
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.Properties
+import javax.mail.Authenticator
+import javax.mail.Message
+import javax.mail.PasswordAuthentication
+import javax.mail.Session
+import javax.mail.Transport
+import javax.mail.internet.InternetAddress
+import javax.mail.internet.MimeMessage
+
+class EmailLogger(
+    private val smtpConfig: SmtpConfig,
+) : ILogger {
+
+    companion object {
+        private const val EMAIL_LOGGER_TAG = "EmailLogger"
+    }
+
+    private lateinit var context: Context
+    private val logBuffer = StringBuilder()
+    private var logCount = 0
+    private var lastSentTime = System.currentTimeMillis()
+    private var lastFailedAttempt = 0L
+    private val retryDelayMillis = 5 * 60 * 1000L
+
+    @Volatile
+    private var isSending = false
+
+    override fun initialize(
+        context: Context,
+        config: LogConfig,
+    ) {
+        this.context = context.applicationContext
+    }
+
+    override fun log(logEntry: LogEntry) {
+        logBuffer.appendLine("[${logEntry.logType}] ${logEntry.tag}: ${logEntry.message}")
+        logEntry.throwable?.let {
+            logBuffer.appendLine(Log.getStackTraceString(it))
+        }
+
+        when (smtpConfig.thresholdType) {
+            ThresholdType.Counter -> {
+                logCount++
+                if (logCount >= smtpConfig.logCountThreshold) {
+                    sendLogsViaSmtp { result, message ->
+                        handleSendResult(
+                            result = result,
+                            message = message,
+                            onSuccess = { logCount = 0 }
+                        )
+                    }
+                }
+            }
+
+            ThresholdType.Timer -> {
+                val now = System.currentTimeMillis()
+                if (now - lastSentTime >= smtpConfig.timeThresholdMillis && // Check for interval
+                    now - lastFailedAttempt >= retryDelayMillis // Check for failed attempts
+                ) {
+                    sendLogsViaSmtp { result, message ->
+                        handleSendResult(
+                            result = result,
+                            message = message,
+                            onSuccess = { lastFailedAttempt = now },
+                            onFailure = { lastFailedAttempt = now }
+                        )
+                    }
+                }
+            }
+        }
+
+    }
+
+    private fun handleSendResult(
+        result: Boolean,
+        message: String?,
+        onSuccess: () -> Unit,
+        onFailure: () -> Unit = {},
+    ) {
+        isSending = false
+        if (result) {
+            onSuccess()
+        } else {
+            onFailure()
+            Log.e(EMAIL_LOGGER_TAG, "SMTP failed: $message")
+        }
+    }
+
+    fun sendLogsViaSmtp(onResult: (Boolean, String?) -> Unit) {
+        if (!isSending) {
+            isSending = true
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    val props = Properties().apply {
+                        put("mail.smtp.auth", "true")
+                        put("mail.smtp.starttls.enable", if (smtpConfig.useSSL) "true" else "false")
+                        put("mail.smtp.host", smtpConfig.smtpHost)
+                        put("mail.smtp.port", smtpConfig.smtpPort.toString())
+                    }
+
+                    val session = Session.getInstance(props, object : Authenticator() {
+                        override fun getPasswordAuthentication(): PasswordAuthentication {
+                            return PasswordAuthentication(
+                                smtpConfig.senderEmail,
+                                smtpConfig.senderPassword
+                            )
+                        }
+                    })
+
+                    val message = MimeMessage(session).apply {
+                        setFrom(InternetAddress(smtpConfig.senderEmail))
+                        setRecipients(
+                            Message.RecipientType.TO,
+                            InternetAddress.parse(smtpConfig.recipientEmail)
+                        )
+                        subject = "App Logs"
+                        setText(logBuffer.toString())
+                    }
+
+                    Transport.send(message)
+                    logBuffer.clear()
+                    onResult(true, null)
+                } catch (e: Exception) {
+                    onResult(false, e.message)
+                }
+            }
+        }
+    }
+}
---
+++ LogFactory/src/main/java/com/sali/logfactory/models/SmtpConfig.kt
@@ -0,0 +1,17 @@
+package com.sali.logfactory.models
+
+data class SmtpConfig(
+    val thresholdType: ThresholdType,
+    val logCountThreshold: Int = 10,
+    val timeThresholdMillis: Long = 5 * 60 * 1000L, // 5 minutes
+    val smtpHost: String = "smtp.gmail.com",
+    val smtpPort: Int = 587,
+    val senderEmail: String,
+    val senderPassword: String,
+    val recipientEmail: String,
+    val useSSL: Boolean = true,
+)
+
+enum class ThresholdType {
+    Counter, Timer
+}
