f2be2d17f4cb9cc7d1736a6d83832b623a7c6a59
Ali Nabavi <arz1379n@gmail.com>
2025-07-27 16:04:27 +0330

    Implement emailLogger object in application class

--- app/src/main/java/com/sali/logfactory/LogFactoryApp.kt
+++ app/src/main/java/com/sali/logfactory/LogFactoryApp.kt
@@ -3,14 +3,32 @@ package com.sali.logfactory
 import android.app.Application
 import com.sali.logfactory.factory.LogFactory
 import com.sali.logfactory.formatter.LogMessageFormatter
+import com.sali.logfactory.logger.EmailLogger
 import com.sali.logfactory.logger.FileLogger
 import com.sali.logfactory.models.LogConfig
 import com.sali.logfactory.models.LogEntry
+import com.sali.logfactory.models.SmtpConfig
+import com.sali.logfactory.models.ThresholdType
 
 class LogFactoryApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        val emailLogger = EmailLogger(
+            smtpConfig = SmtpConfig(
+                thresholdType = ThresholdType.Counter, // Specify which type of threshold you want to use
+                logCountThreshold = 3, // For ThresholdType.Counter type you can specify after how many logs should be sent to the email address
+                // timeThresholdMillis = 5 * 1000 * 60, You can use time threshold if you choose ThresholdType.Timer
+                senderEmail = "sender@gmail.com",
+                senderPassword = "sender password",
+                recipientEmail = "recepient@gmail.com",
+            ),
+            // Use custom log message formatter
+            // formatter = CustomFormatter()
+        )
+        val fileLogger = FileLogger()
+
         LogFactory.configureLoggers(
             context = this,
             config = LogConfig(
@@ -19,7 +37,7 @@ class LogFactoryApp : Application() {
                 // Clear the old logs every time app launched
                 // clearFileWhenAppLaunched = true
             ),
-            enabledLoggers = arrayOf(FileLogger())
+            enabledLoggers = arrayOf(fileLogger, emailLogger)
         )
     }
 
